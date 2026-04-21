77b63d543d14f7a1e319c1a778e84f07b8119c64
Yu Han <coralhanyu@outlook.com>
2026-04-21 16:45:41 +0100

    add second test

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -1,2 +1,12 @@
+from datetime import date
+
+
 def age_checker(date_of_birth):
-    return "Access granted"
+    date_object = date.fromisoformat(date_of_birth)
+    year = date_object.year
+    current_year = 2026
+    age = current_year - year
+    if age < 16:
+        return f"Access denied. You are {age}, and the required age is 16."
+    elif age >= 16:
+        return "Access granted"
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -1,6 +1,12 @@
 from lib.age_checker import *
+import datetime
 
 def test_age_over_16():
     result = age_checker('2001-07-08')
     assert result == "Access granted"
+
+def test_age_under_16():
+    result = age_checker("2011-01-01")
+    assert result == "Access denied. You are 15, and the required age is 16."
+
 
